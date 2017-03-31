f84645656c6152069c27fca269e9f776a46bbc32
Riska Adhita <risk.adhita@gmail.com>
2017-03-31 16:06:03 +0700

    Backend historymurid

---
+++ application/controllers/historymurid_controller.php
@@ -0,0 +1,34 @@
+<?php
+	class historymurid_controller extends CI_Controller
+	{
+		function __construct()
+		{
+			parent::__construct();
+			$this->load->model('historymurid_model');
+		}
+		public function index()
+		{
+			$this->load->database();
+			$username = $_POST['username'];
+			$history = $this->historymurid_model->getHistory($username);
+			// $result = $history;
+			$result = array();
+			$penghubung="--";
+			foreach ($history as $row ) {
+				$tanggal=$row->tanggal_pencarian.$penghubung.$row->jam_pencarian;
+				array_push($result,array(
+		        'id_history'=>$row->id_historytransaksi,
+		        'id_pencariantutor'=>$row->id_pencarian,
+		        'nama_tutor'=>$row->nama_user,
+		        'telp_tutor'=>$row->telp_user,
+		        'pelajaran'=>$row->pelajaran_pencarian,
+		        'tanggal'=>$tanggal,
+		        'biaya'=>$row->biayatutor_pencarian,
+		        'rating'=>$row->rating_tutor,
+		        'komentar'=>$row->komentar_tutor
+		    	));
+			}
+			echo json_encode(array('result'=>$result));
+		}
+	}
+?>
---
+++ application/models/historymurid_model.php
@@ -0,0 +1,19 @@
+<?php
+	class historymurid_model extends CI_Model
+	{
+		function __construct()
+		{
+			parent::__construct();
+		}
+		public function getHistory($username)
+		{
+			$history = "SELECT * FROM `pencarian_tutor` pt, `history_transaksi` ht, `user` u WHERE pt.`id_pencarian`=ht.`id_pencariantutor` AND pt.`username_pencarian` = '$username' AND ht.`username_tutor` = u.`username_user`";
+			// $usernametutor = "SELECT `username_tutor` FROM `history_transaksi` WHERE ``"
+			// $hasil = array();
+			// $hasil['user'] = $this->db->query($history)->result();
+			$hasil = $this->db->query($history);
+			return $hasil->result();
+		}
+
+	}
+?>
